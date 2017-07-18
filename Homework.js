412c62c71033ca5975a31cc0020984c1b6566fdc
ErlynLD <erlyn-x@hotmail.com>
2017-07-18 06:53:40 -0600

    Delete Homework.js

--- Homework.js
+++
@@ -1,7 +0,0 @@
-var nombre = "Erlyn Lebrón Díaz";
-var profesion = "Estudiante";
-var edad = 22;
-var conProgramacion = 2;
-var expectativas = "Conocer sobre el desarrollo agil y eficiente de proyectos Web";
-
-Console.log("Nombre: ", nombre);
