3dc99529516c46418b2c31f919859dcc9b3a4c35
ErlynLD <erlyn-x@hotmail.com>
2017-07-18 06:54:08 -0600

    Add files via upload

---
+++ Homework.js
@@ -0,0 +1,19 @@
+(
+function()
+{
+    'use strict'
+
+    var nombre = "Erlyn Lebrón Díaz";
+    var profesion = "Estudiante";
+    var edad = 22;
+    var conProgramacion = 2;
+    var expectativas = "Conocer sobre el desarrollo ágil y eficiente de proyectos Web";
+
+    console.log("Nombre: ", nombre);
+    console.log("Profesión: ", profesion);
+    console.log("Edad: ", edad);
+    console.log("Conocimientos de programación: ", conProgramacion);
+    console.log("Expectativas del curso: ", expectativas);
+    
+})();
+
