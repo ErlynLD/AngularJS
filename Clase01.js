bc6abdbbb8654963c564dc4788bf1004bb853e36
ErlynLD <erlyn-x@hotmail.com>
2017-07-22 16:11:08 -0600

    Probando otro commit

---
+++ Clase01.js
@@ -0,0 +1,60 @@
+Profesor José Peña
+Ing. Sistemas - APEC
+4 años trabajando en el área
+Aplicaciones Web, .Net, Angular, y BD
+
+Será un curso básico. FrontEnd
+
+Primer Módulo: JavaScript
+
+Usaremos VS Code
+Hay que usar github
+Usar git
+Usar nodejs
+
+Escribir en el cmd
+
+code . - Es para que cuando abra VS Code, éste se abra directamente en ésta carpeta.
+Carpeta dentro de VS Code: Client, dentro readmed.md (markdown) 
+
+Buscar sintaxis de markdown
+
+
+---- dentro del readmed.md
+## FrontEnd con AngularJS
+Curso práctico de AngularJS
+
+
+Crear cuenta github
+Crear repositorio ahí y copiar los comandos de la segunda fila de comandos
+
+
+
+LINK PARA LO QUE HAGA EL PROFESOR
+
+INVESTIGAR QUÉ ES JS 2015 Y JS 2016
+----
+	Declaración de variables
+
+
+	todos los archivos tendrán un 
+
+	(function () {
+		'use strict'
+
+
+	})
+
+
+	Console.log, Console.error, Console.warn
+	Verificar si hay más funcioes de console.
+
+
+	Funcion typeof();
+
+	Los objetos se declaran con llaves.
+
+	Agregar colaborador en settings
+
+	Proyecto del Profesorf
+	https://github.com/josealvis/cursoPracticoAngularJs
---
+++ Clase02.js
@@ -0,0 +1,18 @@
+
+
+//Clase 22/7/2017
+
+/* 
+    Operadores
+        Aritméticos
+        Asignación
+        Post y Pre incremento
+
+        COMPARACION
+            == valores iguales
+            === valor y tipo iguales
+            != valores diferentes
+            != valores o tipos diferentes
+            (...)
+        
+*/
