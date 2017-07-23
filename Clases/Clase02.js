89c8a7500bdae3f7090dc03c5c413230d39c7153
ErlynLD <erlyn-x@hotmail.com>
2017-07-22 18:12:11 -0600

    Confirmar cambios

---
+++ Clases/Clase02.js
@@ -0,0 +1,55 @@
+
+
+//Clase 22/7/2017
+
+/* 
+    Operadores
+        Aritméticos
+        Asignación
+        Post y Pre incremento
+
+        COMPARACION
+            == valores iguales
+            === valor y tipo iguales
+            != valores diferentes
+            != valores o tipos diferentes
+            (...)
+        
+        LOGICOS
+
+        TERNARIO
+
+    CONTROL IF
+    CONTROL SWITCH
+    
+    eloquentjavascript.net
+
+    FUNCIONES ARRAY
+        push
+        splice
+
+    BUCLES
+        FOR
+        WHILE
+    
+    FUNCIONES
+        Anónimas y nombrada
+        recursivas
+
+*/
+
+(
+    function(){
+        'use strict'
+        var arrayNum = [2,4,6,7,2,33,6];
+
+        //Agregamos un valor, y capturamos el nuevo tamaño del arreglo
+        var tamanio = arrayNum.push(10);
+
+        //Aplicar splice
+
+        //Aplicar map
+
+
+    }
+)();
