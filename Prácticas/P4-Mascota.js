75afeb53144c221a9f09b28b3999e6b0a63c8a26
ErlynLD <erlyn-x@hotmail.com>
2017-08-05 13:41:48 -0600

    Prac4

---
+++ Prácticas/P4-Mascota.js
@@ -0,0 +1,30 @@
+(function(){
+    'use strict'
+
+    function Perro(pNombre, pRaza, pAnoNac, pSexo){
+        this.nombre = pNombre;
+        this.raza = pRaza;
+        this.anoNac = pAnoNac;
+        this.sexo = pSexo;
+
+        this.Ladrar = function(){
+            console.log("Grrrr, Grrrrr, Hau Hau!!");
+        }, 
+        this.Comer = function(){
+            console.log("Perro comiendo");
+        }, 
+        this.getEdad = function(){
+            console.log((new Date().getFullYear - this.anoNac) + " años.");
+        },
+        this.getNombreYRaza = function(){
+            console.log("Nombre: " + this.nombre + "\nRaza: " + this.raza);
+        },
+        this.getSexo = function(){
+            console.log("Sexo: " + this.sexo);
+        }
+    }
+
+    var perro1 = new Perro("Lazy", "Pastor Alemán", 2003, 'F');
+    perro1.getNombreYRaza();
+    
+})();
